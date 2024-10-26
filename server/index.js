7f543d93feed6c9d0e985f986043daa63b461e49
Himaja Narajala <hnarajal@purdue.edu>
2024-10-26 13:23:59 -0400

    Test commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from "express";
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("Hello!")
+});
+
 app.listen(8000, () => {
     console.log("Listening to port 8000")
-} );
+});
 
